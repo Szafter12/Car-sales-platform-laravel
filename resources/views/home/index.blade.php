e146d1c93fbf060bed58dd41d091467a9f8905e4
Jakub <jakubpachut@gmail.com>
2025-04-01 12:41:04 +0200

    displaying data on website

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -49,38 +49,18 @@
                 </div>
             </div>
             <button type="button" class="hero-slide-prev">
-                <svg
-                    style="width: 18px"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M5 1 1 5l4 4"
-                    />
+                <svg style="width: 18px" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M5 1 1 5l4 4" />
                 </svg>
                 <span class="sr-only">Previous</span>
             </button>
             <button type="button" class="hero-slide-next">
-                <svg
-                    style="width: 18px"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m1 9 4-4-4-4"
-                    />
+                <svg style="width: 18px" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 9 4-4-4-4" />
                 </svg>
                 <span class="sr-only">Next</span>
             </button>
@@ -94,9 +74,9 @@
             <div class="container">
                 <h2>Latest Added Cars</h2>
                 <div class="car-items-listing">
-                    @for($i = 0; $i < 12; $i++)
-                        <x-car-item/>
-                    @endfor
+                    @foreach ($cars as $car)
+                        <x-car-item :$car/>
+                    @endforeach
                 </div>
             </div>
         </section>
--- resources/views/layout/app.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['title' => '', 'footerLinks' => ''])
-<x-base-layout :title="$title">
-    <x-layout-ui.header />
-    {{ $slot }}
-</x-base-layout>
