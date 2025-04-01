7ff86c89c6ab8ce6296f5b1b5587f623e35050d4
Jakub <jakubpachut@gmail.com>
2025-04-02 00:45:08 +0200

    displaying data on car/show page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -75,7 +75,7 @@
                 <h2>Latest Added Cars</h2>
                 <div class="car-items-listing">
                     @foreach ($cars as $car)
-                        <x-car-item :$car/>
+                        <x-car-item :car="$car" />
                     @endforeach
                 </div>
             </div>
