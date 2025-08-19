d7880882b3ae92328c43970b51a127bfeac2264c
Jakub Pachut <jakubpachut@gmail.com>
2025-08-19 23:19:12 +0200

    adding more tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -17,14 +17,30 @@ class AuthTest extends TestCase
     {
         $response = $this->get(route("login"));
 
-        $response->assertOk();
+        $response->assertOk()
+            ->assertSee("Login")
+            ->assertSee("Forgot password?")
+            ->assertSee("Signup for free")
+            ->assertSee("Facebook")
+            ->assertSee("Google")
+            ->assertSee('<a href="'.route('password.request').'"', false)
+            ->assertSee('<a href="'.route('login.oauth', 'google').'"', false)
+            ->assertSee('<a href="'.route('login.oauth', 'facebook').'"', false)
+            ->assertSee('<a href="'.route('signup').'"', false);
     }
 
     public function test_should_return_success_on_signup_page(): void
     {
         $response = $this->get(route("signup"));
 
-        $response->assertOk();
+        $response->assertOk()
+            ->assertSee("Sign up")
+            ->assertSee("Click here to login")
+            ->assertSee("Facebook")
+            ->assertSee("Google")
+            ->assertSee('<a href="'.route('login.oauth', 'google').'"', false)
+            ->assertSee('<a href="'.route('login.oauth', 'facebook').'"', false)
+            ->assertSee('<a href="'.route('login').'"', false);
     }
 
     public function test_should_return_success_on_forgot_password_page(): void
