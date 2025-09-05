228b4a8fee9dd93650799890eca9b1fe18128335
Jakub Pachut <jakubpachut@gmail.com>
2025-09-06 00:55:47 +0200

    change cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $cars = Cache::remember('home-cars', 60, function () {
+        $cars = Cache::remember('home-cars', Carbon::now()->addMinute(), function () {
             return Car::where('published_at', '<=', now())
                 ->with(['primaryImage', 'city', 'maker', 'model', 'carType', 'fuelType', 'favouredUsers'])
                 ->orderBy('published_at', 'desc')
